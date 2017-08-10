6952c069f3893503d0ae080dc460cd94c8d91010
rajandhaliwal80 <rdhali36@my.centennialcollege.ca>
2017-08-10 13:43:37 -0400

    Created Radio Buttons

--- BMICalculator/Form1.Designer.cs
+++ BMICalculator/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.AppLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.Imperial = new System.Windows.Forms.RadioButton();
+            this.Metric = new System.Windows.Forms.RadioButton();
+            this.AppLayout.SuspendLayout();
             this.SuspendLayout();
             // 
             // AppLayout
@@ -37,6 +40,8 @@
             this.AppLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.AppLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.AppLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.AppLayout.Controls.Add(this.Metric, 1, 0);
+            this.AppLayout.Controls.Add(this.Imperial, 0, 0);
             this.AppLayout.Location = new System.Drawing.Point(1, 1);
             this.AppLayout.Name = "AppLayout";
             this.AppLayout.RowCount = 6;
@@ -48,6 +53,31 @@
             this.AppLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.AppLayout.Size = new System.Drawing.Size(319, 431);
             this.AppLayout.TabIndex = 0;
+            // 
+            // Imperial
+            // 
+            this.Imperial.AutoSize = true;
+            this.Imperial.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Imperial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Imperial.Location = new System.Drawing.Point(3, 3);
+            this.Imperial.Name = "Imperial";
+            this.Imperial.Size = new System.Drawing.Size(97, 28);
+            this.Imperial.TabIndex = 0;
+            this.Imperial.TabStop = true;
+            this.Imperial.Text = "Imperial";
+            this.Imperial.UseVisualStyleBackColor = true;
+            // 
+            // Metric
+            // 
+            this.Metric.AutoSize = true;
+            this.Metric.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Metric.Location = new System.Drawing.Point(109, 3);
+            this.Metric.Name = "Metric";
+            this.Metric.Size = new System.Drawing.Size(82, 28);
+            this.Metric.TabIndex = 1;
+            this.Metric.TabStop = true;
+            this.Metric.Text = "Metric";
+            this.Metric.UseVisualStyleBackColor = true;
             // 
             // BMICalculator
             // 
@@ -63,6 +93,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BMI Calculator";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.AppLayout.ResumeLayout(false);
+            this.AppLayout.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +102,8 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel AppLayout;
+        private System.Windows.Forms.RadioButton Metric;
+        private System.Windows.Forms.RadioButton Imperial;
     }
 }
 
