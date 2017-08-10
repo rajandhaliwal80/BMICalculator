5676acdd9b5c077a42e7486076f4699f591211bf
rajandhaliwal80 <rdhali36@my.centennialcollege.ca>
2017-08-10 18:23:30 -0400

    Created A splash Form

---
+++ BMICalculator/SplashForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/// <summary>
+/// Rajandeep Kaur Dhaliwal - 300926123
+/// Splash Form
+/// </summary>
+namespace BMICalculator
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
